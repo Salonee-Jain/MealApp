4098c6e89516acdea5790849ab1fc9cc1eeaf507
Salonee-Jain <salonijain1248@gmail.com>
2023-01-30 23:36:26 +0530

    added favicon

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function showMealList() {
             <div class="page-wrap d-flex flex-row align-items-center">
                 <div class="container">
                     <div class="row justify-content-center">
-                        <div class="col-md-12 text-center">
+                        <div class="col-md-12 text-center" ">
                             <span class="display-1 d-block">404</span>
                             <div class="mb-4 lead">
                                 The meal you are looking for was not found.
